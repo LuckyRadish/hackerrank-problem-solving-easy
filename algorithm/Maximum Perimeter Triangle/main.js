628127aff5dbd3a4498b90c61ae600156edb87d0
LuckyRadish <lucky.radish.x@gmail.com>
2022-12-22 23:23:14 +0900

    Resolve "Maximum Perimeter Triangle"

---
+++ algorithm/Maximum Perimeter Triangle/main.js
@@ -0,0 +1,14 @@
+function maximumPerimeterTriangle(sticks) {
+  // Time complexity - O(nlog n), n is |sticks|
+
+  sticks.sort((a, b) => b - a);
+
+  for (let i = 0; i < sticks.length - 2; i++) {
+    const [a, b, c] = [sticks[i], sticks[i + 1], sticks[i + 2]];
+    if (a < b + c) {
+      return [c, b, a];
+    }
+  }
+
+  return [-1];
+}
