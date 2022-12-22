d9f3f2621b2095a497411547504b003d5101de0a
LuckyRadish <lucky.radish.x@gmail.com>
2022-12-22 21:26:07 +0900

    Resolve "Divisible Sum Pairs"

---
+++ algorithm/Divisible Sum Pairs/main.js
@@ -0,0 +1,15 @@
+function divisibleSumPairs(n, k, ar) {
+  // Time complexity - O(n²)
+
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
